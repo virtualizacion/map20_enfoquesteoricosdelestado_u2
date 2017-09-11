eb6bd8be3c466a976396f9e6e3b415eece2a552c
johanmorales <jhellmetal2000@gmail.com>
2017-09-11 17:24:16 -0500

    Primer commit infografía pendiente de revisión esap.

---
+++ js/infografia.js
@@ -0,0 +1,8 @@
+$("document").ready(function(){
+    
+    $(".btntema1, .btntema2, .btntema3, .btntema4, .btntema5, .btntema6, .btntema7, .btntema8, .btntema9").click(function(){
+        $(this).css("opacity", ".5");
+    });
+
+    
+});
